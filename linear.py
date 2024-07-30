30f432430a4a599e81c5293c3fe0a79950f47cb8
samuelVRcoder <118441330+samuelVRcoder@users.noreply.github.com>
2024-07-30 18:13:26 -0300

    Update linear.py

--- linear.py
+++ linear.py
@@ -2,11 +2,11 @@ import sklearn.datasets as dt
 
 from sklearn.linear_model import LinearRegression as lr
 
-model = lr()
-
 from sklearn.model_selection import train_test_split as tts
 
-X, y = dt.make_regression()
+model = lr()
+
+X, y = dt.make_regression(n_samples=30000)
 
 xtr, xts, ytr, yts = tts(X, y, random_state=42)
 
